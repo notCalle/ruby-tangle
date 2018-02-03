72cef2884809ea0106ae67b779467e4c342a36a9
Calle Englund <calle@discord.bofh.se>
2018-02-03 11:56:53 +0100

    Add basic Edge class
    An edge can connect two vertices, or loop one vertex.

---
+++ spec/tangle_edge_spec.rb
@@ -0,0 +1,27 @@
+require 'tangle/vertex'
+require 'tangle/edge'
+
+RSpec.describe Tangle::Edge do
+  before :example do
+    @vertex1 = Tangle::Vertex.new
+    @vertex2 = Tangle::Vertex.new
+    @edge = Tangle::Edge.new(@vertex1, @vertex2)
+  end
+
+  it 'is a simple object' do
+    expect(@edge).to be_a Object
+  end
+
+  it 'connects vertices' do
+    expect(@edge).to respond_to :vertices
+    [@vertex1, @vertex2].each do |vertex|
+      expect(@edge).to include vertex
+    end
+  end
+
+  it 'can be walked from one vertex to another' do
+    expect(@edge).to respond_to :walk
+    expect(@edge.walk(@vertex1)).to be @vertex2
+    expect(@edge.walk(@vertex2)).to be @vertex1
+  end
+end
