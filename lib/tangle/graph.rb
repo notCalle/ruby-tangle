b2cb8e7cc627aed9f1b1310d9770c3d69ef31441
Calle Englund <calle@discord.bofh.se>
2018-02-05 17:20:09 +0100

    Add (dis)connectedness methods to graph
    Add methods to test (dis)connectedness of a graph, and return the largest (dis)connected subgraph for a vertex in the graph.

--- lib/tangle/graph.rb
+++ lib/tangle/graph.rb
@@ -108,6 +108,43 @@ module Tangle
     end
     alias dup subgraph
 
+    # Get the largest connected subgraph for a vertex.
+    # Also aliased as :component and :connected_component
+    #
+    # connected_subgraph(vertex) => Graph
+    #
+    def connected_subgraph(vertex)
+      subgraph { |other| vertex.connected?(other) }
+    end
+    alias component connected_subgraph
+    alias connected_component connected_subgraph
+
+    # Get the largest subgraph that is not connected to a vertex, or what's
+    # left after removing the connected subgraph.
+    #
+    def disconnected_subgraph(vertex)
+      subgraph { |other| !vertex.connected?(other) }
+    end
+
+    # A graph is connected if all vertices are connected to all vertices
+    # An empty graph is disconnected.
+    #
+    def connected?
+      return false if vertices.empty?
+
+      vertices.combination(2).all? do |pair|
+        this, that = pair.to_a
+        this.connected?(that)
+      end
+    end
+
+    # A graph is disconnected if any vertex is not connected to all other.
+    # An empty graph is disconnected.
+    #
+    def disconnected?
+      !connected?
+    end
+
     protected
 
     # Insert a prepared vertex into the graph
