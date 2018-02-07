3ce2ca0e5d69aee96b87b44e73a7bc2ae361878d
Calle Englund <calle@discord.bofh.se>
2018-02-07 11:46:39 +0100

    Update vertex specs for digraphs
    Add spec for vertex behaviour in a directed graph.

--- spec/tangle_vertex_spec.rb
+++ spec/tangle_vertex_spec.rb
@@ -68,4 +68,89 @@ RSpec.describe Tangle::Vertex do
       expect(@vertex_a.connected?(@vertex_d)).to be true
     end
   end
+
+  context 'when in a directed graph' do
+    before :context do
+      @graph = Tangle::DiGraph.new
+      @vertex_a = @graph.add_vertex(name: 'a')
+      @vertex_b = @graph.add_vertex(name: 'b')
+      @graph.add_edge 'a', 'b'
+      @vertex_c = @graph.add_vertex(name: 'c')
+      @graph.add_edge 'b', 'c'
+    end
+
+    it 'can find its parents' do
+      expect(@vertex_a).to respond_to :parents
+    end
+
+    it 'can find its children' do
+      expect(@vertex_a).to respond_to :children
+    end
+
+    it 'only includes direct ancestors in parents' do
+      expect(@vertex_a.parents).to include @vertex_b
+      expect(@vertex_a.parents).not_to include @vertex_c
+    end
+
+    it 'only includes direct descendants in children' do
+      expect(@vertex_c.children).to include @vertex_b
+      expect(@vertex_c.children).not_to include @vertex_a
+    end
+
+    it 'can test parentness' do
+      expect(@vertex_a).to respond_to :parent?
+    end
+
+    it 'can test childness' do
+      expect(@vertex_a).to respond_to :child?
+    end
+
+    it 'is only a parent to its direct descendants' do
+      expect(@vertex_a.parent?(@vertex_b)).to be true
+      expect(@vertex_a.parent?(@vertex_c)).to be false
+    end
+
+    it 'is only a child to its direct ancestors' do
+      expect(@vertex_c.child?(@vertex_b)).to be true
+      expect(@vertex_c.child?(@vertex_a)).to be false
+    end
+
+    it 'can test ancestry' do
+      expect(@vertex_a).to respond_to :ancestor?
+    end
+
+    it 'can test descendancy' do
+      expect(@vertex_a).to respond_to :descendant?
+    end
+
+    it 'is an ancestor to itself' do
+      expect(@vertex_a.ancestor?(@vertex_a)).to be true
+    end
+
+    it 'is a descendant to itself' do
+      expect(@vertex_a.descendant?(@vertex_a)).to be true
+    end
+
+    it 'is an ancestor to its children' do
+      expect(@vertex_a.ancestor?(@vertex_b)).to be true
+    end
+
+    it 'is a descendant of its parents' do
+      expect(@vertex_b.descendant?(@vertex_a)).to be true
+    end
+
+    it 'is an ancestor through transitive parentness' do
+      expect(@vertex_a.parent?(@vertex_c)).to be false
+      expect(@vertex_a.parent?(@vertex_b)).to be true
+      expect(@vertex_b.parent?(@vertex_c)).to be true
+      expect(@vertex_a.ancestor?(@vertex_c)).to be true
+    end
+
+    it 'is a descendant through transitive childness' do
+      expect(@vertex_c.child?(@vertex_a)).to be false
+      expect(@vertex_c.child?(@vertex_b)).to be true
+      expect(@vertex_b.child?(@vertex_a)).to be true
+      expect(@vertex_c.descendant?(@vertex_a)).to be true
+    end
+  end
 end
