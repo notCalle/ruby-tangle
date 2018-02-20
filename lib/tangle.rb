9bd994f71cec265abeabebd9e80d7e429de88034
Calle Englund <calle@discord.bofh.se>
2018-02-20 11:23:46 +0100

    Add class alias Tangle::DAG
    Short name for Directed::Acyclic::Graph.

--- lib/tangle.rb
+++ lib/tangle.rb
@@ -3,6 +3,7 @@ require 'tangle/errors'
 require 'tangle/graph'
 require 'tangle/simple/graph'
 require 'tangle/directed/graph'
+require 'tangle/directed/acyclic/graph'
 
 # Tangle manages various types of graphs
 #
@@ -12,8 +13,15 @@ require 'tangle/directed/graph'
 # Tangle::SimpleGraph.new
 # => Undirected graph with single edges between vertices, and no loops
 #
+# Tangle::DiGraph.new
+# => Directed graph without edge constraints
+#
+# Tangle::DAG.new
+# => Directed graph with no edge cycles
+#
 module Tangle
   MultiGraph = Tangle::Graph
   SimpleGraph = Tangle::Simple::Graph
   DiGraph = Tangle::Directed::Graph
+  DAG = Tangle::Directed::Acyclic::Graph
 end
