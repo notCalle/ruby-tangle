a52c7bcedd0718aa5693114994c23510a5ce8e8e
Calle Englund <calle@discord.bofh.se>
2018-04-05 12:39:46 +0200

    Add specs for mixins
    Add tests for mixin method visibility and kwarg initialization.

---
+++ spec/tangle_mixin_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe Tangle::Mixin do
+  it 'is a module' do
+    expect(Tangle::Mixin).to be_a Module
+  end
+
+  context 'a mixin' do
+    before :context do
+      @mixins = [Helpers::TestMixin]
+    end
+
+    it 'can provide methods' do
+      expect(Tangle::Graph.new(mixins: @mixins)).to respond_to :mixin_ok?
+    end
+
+    it 'can provide keyword initializers' do
+      expect {
+        Tangle::Graph.new(mixins: @mixins, mixin_ok: true)
+      }.not_to raise_error
+
+      expect {
+        Tangle::Graph.new(mixins: @mixins, mixin_broken: true)
+      }.to raise_error NoMethodError
+    end
+  end
+end
