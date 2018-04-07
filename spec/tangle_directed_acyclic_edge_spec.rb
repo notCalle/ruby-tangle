cf9fa2cf3adbd6eeaa197a828e2d2d0997d628fb
Calle Englund <calle@discord.bofh.se>
2018-04-07 19:10:30 +0200

    Add spec for DAG multipath to an ancestor

--- spec/tangle_directed_acyclic_edge_spec.rb
+++ spec/tangle_directed_acyclic_edge_spec.rb
@@ -3,9 +3,11 @@ require 'tangle/directed/acyclic/graph'
 RSpec.describe Tangle::Directed::Acyclic::Edge do
   before :context do
     @graph = Tangle::Directed::Acyclic::Graph.new
-    @vtx1 = @graph.add_vertex name: 'a'
-    @vtx2 = @graph.add_vertex name: 'b'
+    @graph.add_vertex name: 'a'
+    @graph.add_vertex name: 'b'
+    @graph.add_vertex name: 'c'
     @edge = @graph.add_edge 'a', 'b'
+    @graph.add_edge 'b', 'c'
   end
 
   it 'is a specialization of a directed edge' do
@@ -14,7 +16,11 @@ RSpec.describe Tangle::Directed::Acyclic::Edge do
 
   it 'disallows edge cycles' do
     expect { @graph.add_edge('a') }.to raise_error Tangle::CyclicError
-    expect { @graph.add_edge('b', 'a') }.to raise_error Tangle::CyclicError
+    expect { @graph.add_edge('c', 'a') }.to raise_error Tangle::CyclicError
+  end
+
+  it 'allows multiple paths to an ancestor' do
+    expect { @graph.add_edge('a', 'c') }.not_to raise_error
   end
 
   it 'allows multiedges' do
