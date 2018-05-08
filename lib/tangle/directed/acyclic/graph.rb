bb50c125aa63b6a905c10f67f2e135ec9cea9f05
Calle Englund <calle@discord.bofh.se>
2018-05-08 16:46:36 +0200

    Add topological ordering to DAG
    Create a topological ordering of the vertices in a DAG by sorting them in their partial order.

--- lib/tangle/directed/acyclic/graph.rb
+++ lib/tangle/directed/acyclic/graph.rb
@@ -1,11 +1,22 @@
 require 'tangle/directed/graph'
+require 'tangle/directed/acyclic/partial_order'
 
 module Tangle
   module Directed
     module Acyclic
-      #
       # A directed acyclic graph
       class Graph < Tangle::Directed::Graph
+        # Return a topological ordering of a set of vertices, or all
+        # vertices in the graph.
+        def topological_ordering(*vertices)
+          vertices = if vertices.empty?
+                       self.vertices
+                     else
+                       get_vertices(*vertices) unless vertices.empty?
+                     end
+          PartialOrder[self, vertices].sort!.map(&:vertex)
+        end
+
         protected
 
         def insert_edge(edge)
