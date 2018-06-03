36e2a22cb653ca75764cc73f84314aa8d9820dd5
Calle Englund <calle@discord.bofh.se>
2018-06-03 11:54:00 +0200

    Add vertex currifications for DiGraphs

--- lib/tangle/directed/graph.rb
+++ lib/tangle/directed/graph.rb
@@ -12,87 +12,87 @@ module Tangle
       DEFAULT_MIXINS = [].freeze
 
       # Return the incoming edges for +vertex+
-      def in_edges(vertex)
+      currify :vertex, def in_edges(vertex)
         edges(vertex).select { |edge| edge.head?(vertex) }
       end
 
       # Return the direct predecessors of +vertex+
-      def direct_predecessors(vertex)
+      currify :vertex, def direct_predecessors(vertex)
         in_edges(vertex).map(&:tail).to_set
       end
 
       # Is +other+ a direct predecessor of +vertex+?
-      def direct_predecessor?(vertex, other)
+      currify :vertex, def direct_predecessor?(vertex, other)
         direct_predecessors(vertex).include?(other)
       end
 
       # Return a breadth first enumerator for all predecessors
-      def predecessors(vertex)
+      currify :vertex, def predecessors(vertex)
         vertex_enumerator(vertex, :direct_predecessors)
       end
 
       # Is +other+ a predecessor of +vertex+?
-      def predecessor?(vertex, other)
+      currify :vertex, def predecessor?(vertex, other)
         predecessors(vertex).any? { |vtx| other.eql?(vtx) }
       end
 
       # Return a subgraph with all predecessors of a +vertex+
-      def predecessor_subgraph(vertex, &selector)
+      currify :vertex, def predecessor_subgraph(vertex, &selector)
         subgraph(predecessors(vertex), &selector)
       end
 
       # Return the outgoing edges for +vertex+
-      def out_edges(vertex)
+      currify :vertex, def out_edges(vertex)
         edges(vertex).select { |edge| edge.tail?(vertex) }
       end
 
       # Return the direct successors of +vertex+
-      def direct_successors(vertex)
+      currify :vertex, def direct_successors(vertex)
         out_edges(vertex).map(&:head).to_set
       end
 
       # Is +other+ a direct successor of +vertex+?
-      def direct_successor?(vertex, other)
+      currify :vertex, def direct_successor?(vertex, other)
         direct_successors(vertex).include?(other)
       end
 
       # Return a breadth first enumerator for all successors
-      def successors(vertex)
+      currify :vertex, def successors(vertex)
         vertex_enumerator(vertex, :direct_successors)
       end
 
       # Is +other+ a successor of +vertex+?
-      def successor?(vertex, other)
+      currify :vertex, def successor?(vertex, other)
         successors(vertex).any? { |vtx| other.eql?(vtx) }
       end
 
       # Return a subgraph with all successors of a +vertex+
-      def successor_subgraph(vertex, &selector)
+      currify :vertex, def successor_subgraph(vertex, &selector)
         subgraph(successors(vertex), &selector)
       end
 
       # Return the in degree for +vertex+
-      def in_degree(vertex)
+      currify :vertex, def in_degree(vertex)
         in_edges(vertex).count
       end
 
       # Return the out degree for +vertex+
-      def out_degree(vertex)
+      currify :vertex, def out_degree(vertex)
         out_edges(vertex).count
       end
 
       # Is +vertex+ a sink in the graph?
-      def sink?(vertex)
+      currify :vertex, def sink?(vertex)
         out_degree(vertex).zero?
       end
 
       # Is +vertex+ a source in the graph?
-      def source?(vertex)
+      currify :vertex, def source?(vertex)
         in_degree(vertex).zero?
       end
 
       # Is +vertex+ internal in the graph?
-      def internal?(vertex)
+      currify :vertex, def internal?(vertex)
         !(sink?(vertex) || source?(vertex))
       end
 
