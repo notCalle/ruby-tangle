5b112326c3bf11f6c4c48c1973b664f49aec2551
Calle Englund <calle@discord.bofh.se>
2018-06-03 11:45:40 +0200

    Add module to currify methods
    `Tangle::Currify` extends the class that includes it to provide the new `currify(tag, method)` private class method, used for marking instance methods for currying `self`, for use by other objects.
    
    E.g.
    ```
    currify :vertex, def edges(vertex = nil)
    …
    end
    ```

---
+++ lib/tangle/currify.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Tangle
+  module Currify
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      # Return a list of currified methods for a given tag.
+      #
+      # :call-seq:
+      #   self.class.currified_methods(tag) => Array of Symbol
+      def currified_methods(tag)
+        mine = @currified_methods&.[](tag) || []
+        return mine unless superclass.respond_to?(:currified_methods)
+        superclass.currified_methods(tag) + mine
+      end
+
+      private
+
+      # Add a symbol to the list of currified methods for a tag.
+      #
+      # :call-seq:
+      #   class X
+      #     currify :tag, :method
+      def currify(tag, method)
+        @currified_methods ||= {}
+        @currified_methods[tag] ||= []
+        @currified_methods[tag] << method
+      end
+    end
+
+    private
+
+    def define_currified_methods(obj, tag)
+      self.class.currified_methods(tag)&.each do |name|
+        obj.instance_exec(name, method(name).curry) do |method_name, method|
+          define_singleton_method(method_name) do |*args|
+            method.(self, *args)
+          end
+        end
+      end
+    end
+  end
+end
